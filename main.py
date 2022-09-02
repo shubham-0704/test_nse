bdd354d88c5d70d51a9f7095586f4ad64d160224
shubham kumar <66365254+shubham-0704@users.noreply.github.com>
2022-09-02 15:00:38 +0530

    basic_file

---
+++ main.py
@@ -0,0 +1,74 @@
+# Libraries
+import requests
+import json
+import math
+import  datetime as dt
+
+
+m1=0
+
+# Method to get nearest strikes
+def round_nearest(x,num=50): return int(math.ceil(float(x)/num)*num)
+def nearest_strike_bnf(x): return round_nearest(x,100)
+def nearest_strike_nf(x): return round_nearest(x,50)
+
+# Urls for fetching Data
+url_oc      = "https://www.nseindia.com/option-chain"
+url_bnf     = 'https://www.nseindia.com/api/option-chain-indices?symbol=BANKNIFTY'
+url_nf      = 'https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY'
+url_indices = "https://www.nseindia.com/api/allIndices"
+
+# Headers
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+            'accept-language': 'en,gu;q=0.9,hi;q=0.8',
+            'accept-encoding': 'gzip, deflate, br'}
+
+sess = requests.Session()
+cookies = dict()
+
+# Local methods
+def set_cookie():
+    request = sess.get(url_oc, headers=headers, timeout=1000)
+    cookies = dict(request.cookies)
+
+def get_data(url):
+    set_cookie()
+    response = sess.get(url, headers=headers, cookies=cookies)
+    if(response.status_code==401):
+        set_cookie()
+        response = sess.get(url_nf, headers=headers, timeout=5, cookies=cookies)
+    if(response.status_code==200):
+        return response.text
+    return ""
+
+
+# Fetching CE and PE data based on Nearest Expiry Date
+def print_oi(num,step,nearest,url):
+    strike = nearest - (step*num)
+    start_strike = nearest - (step*num)
+    response_text = get_data(url)
+    data = json.loads(response_text)
+    currExpiryDate = data["records"]["expiryDates"][0]
+
+    oi = data["filtered"]["data"][72]["PE"]["changeinOpenInterest"]
+
+
+    s=m1
+
+    if s !=oi:
+        now = dt.datetime.now()
+
+        current_time = now.strftime("%H:%M:%S")
+        print("Current Time =", current_time)
+        s=oi
+        print(s)
+        
+
+    return s
+
+
+
+
+t =10000000000
+while(t>0):
+    m1=print_oi(10,50,17600,url_nf)
